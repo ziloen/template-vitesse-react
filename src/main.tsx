e5e1fc03933f90a6ddd817ed95949bfa8f6c5f32
N <zhupengcheng5@163.com>
2022-10-10 15:23:52 +0800

    chore

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,10 @@
-import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
 
 import '@unocss/reset/tailwind.css'
-import './styles/main.css'
 import 'uno.css'
+import './styles/main.css'
 
 const container = document.querySelector('#root')
 const root = createRoot(container!)
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+root.render(<App />)
