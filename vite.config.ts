f87aa6e0622c49ccf80d9c1ad538527c63ee93bf
N <34268416+ziloen@users.noreply.github.com>
2022-10-15 01:52:31 +0800

    feat: add postcss

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,20 @@
 /// <reference types="vitest" />
-
-import path from 'path'
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import Pages from 'vite-plugin-pages'
-import AutoImport from 'unplugin-auto-import/vite'
+import path from 'node:path'
 import Unocss from 'unocss/vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import { defineConfig } from 'vite'
+import Pages from 'vite-plugin-pages'
+import PostcssPresetEnv from 'postcss-preset-env'
+
 
 export default defineConfig({
   resolve: {
     alias: {
-      '~/': `${path.resolve(__dirname, 'src')}/`,
-    },
+      '~/': `${path.resolve(__dirname, 'src')}/`
+    }
   },
+
   plugins: [
     // https://github.com/antfu/unocss
     // see unocss.config.ts for config
@@ -28,14 +30,20 @@ export default defineConfig({
       imports: [
         'react',
         'react-router-dom',
-        { 'usehooks-ts': ['useCounter', 'useDarkMode'] },
+        { 'usehooks-ts': ['useCounter', 'useDarkMode'] }
       ],
-      dts: true,
-    }),
+      dts: true
+    })
   ],
+
+  css: {
+    postcss: {
+      plugins: [PostcssPresetEnv({ stage: 2 })]
+    }
+  },
 
   // https://github.com/vitest-dev/vitest
   test: {
-    environment: 'jsdom',
-  },
+    environment: 'jsdom'
+  }
 })
