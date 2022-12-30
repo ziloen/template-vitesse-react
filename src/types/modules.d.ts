80526cd91cbe1f32265f675e4e6210bfc9455e50
N <34268416+ziloen@users.noreply.github.com>
2022-12-30 17:50:58 +0800

    chore: types

--- src/types/modules.d.ts
+++ src/types/modules.d.ts
@@ -2,3 +2,10 @@
  * declare or fix module types
  * /// <reference type="" />
  */
+
+declare module "react" {
+  // fix react forwardRef, https://stackoverflow.com/questions/58469229/react-with-typescript-generics-while-using-react-forwardref
+  function forwardRef<T, P = {}>(
+    render: (props: P, ref: ForwardedRef<T>) => ReactElement | null
+  ): (props: P & RefAttributes<T>) => ReactElement | null
+}
