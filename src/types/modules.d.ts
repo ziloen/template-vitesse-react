4a6962543c7ba82c153f2500d648da778d8cc4a8
N <34268416+ziloen@users.noreply.github.com>
2022-12-30 17:51:30 +0800

    chore

--- src/types/modules.d.ts
+++ src/types/modules.d.ts
@@ -3,6 +3,7 @@
  * /// <reference type="" />
  */
 
+import { } from "react"
 declare module "react" {
   // fix react forwardRef, https://stackoverflow.com/questions/58469229/react-with-typescript-generics-while-using-react-forwardref
   function forwardRef<T, P = {}>(
