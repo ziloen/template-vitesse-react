36e2105850fcd749a3db6f2e7d9e02a36e335f97
N <34268416+ziloen@users.noreply.github.com>
2023-03-15 15:59:17 +0800

    chore: allow use custom properties in css style

--- src/types/modules.d.ts
+++ src/types/modules.d.ts
@@ -3,10 +3,15 @@
  * /// <reference type="@types/xxx" />
  */
 
-import React from "react"
-declare module "react" {
+namespace React {
   // fix react forwardRef, https://stackoverflow.com/questions/58469229/react-with-typescript-generics-while-using-react-forwardref
   function forwardRef<T, P = {}>(
     render: (props: P, ref: ForwardedRef<T>) => ReactElement | null
   ): (props: P & RefAttributes<T>) => ReactElement | null
+
+  // allow style to use css custom properties
+  interface CSSProperties {
+    [CSSCutomProperties: `--${string}`]: string
+  }
+
 }
