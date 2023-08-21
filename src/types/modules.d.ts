dab6f8cc8e8da7ce68eb3058aab5a5747b2aa67a
N <34268416+ziloen@users.noreply.github.com>
2023-08-21 12:45:51 +0800

    chore(deps)

--- src/types/modules.d.ts
+++ src/types/modules.d.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-invalid-void-type */
 /* eslint-disable @typescript-eslint/ban-types */
 declare namespace React {
   // fix react forwardRef, https://stackoverflow.com/questions/58469229/react-with-typescript-generics-while-using-react-forwardref
@@ -9,5 +10,6 @@ declare namespace React {
   interface CSSProperties {
     [CSSCutomProperties: `--${string}`]: string | number | undefined
   }
-
+  // allow destructor return value
+  function useEffect(effect: () => (void | (() => void)), deps?: DependencyList): void
 }
