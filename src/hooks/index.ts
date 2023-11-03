67cc4cb260409f1877c63c48d5d9726153bf1994
N <34268416+ziloen@users.noreply.github.com>
2023-11-03 11:57:10 +0800

    Add mergeRefs hook to index.ts

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,3 +1,16 @@
 export * from './useElementBounding'
 export * from './useI18n'
 export * from './useMagicKeys'
+
+
+export function mergeRefs<T>(...refs: React.Ref<T>[]): React.RefCallback<T> {
+  return function (value: T | null) {
+    for (const ref of refs) {
+      if (typeof ref === 'function') {
+        ref(value)
+      } else if (ref) {
+        (ref as React.MutableRefObject<T | null>).current = value
+      }
+    }
+  }
+}
