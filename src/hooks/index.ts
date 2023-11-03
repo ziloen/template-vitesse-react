40fe2816c92c03d17e4eadbf7615244889b5f767
N <34268416+ziloen@users.noreply.github.com>
2023-11-03 11:58:29 +0800

    Refactor mergeRefs function to use more descriptive variable names

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -4,12 +4,12 @@ export * from './useMagicKeys'
 
 
 export function mergeRefs<T>(...refs: React.Ref<T>[]): React.RefCallback<T> {
-  return function (value: T | null) {
+  return function (instance: T | null) {
     for (const ref of refs) {
       if (typeof ref === 'function') {
-        ref(value)
+        ref(instance)
       } else if (ref) {
-        (ref as React.MutableRefObject<T | null>).current = value
+        (ref as React.MutableRefObject<T | null>).current = instance
       }
     }
   }
