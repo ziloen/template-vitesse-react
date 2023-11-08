1d66e3a17f9ce82190b2d0aacfef909cc3786d98
N <34268416+ziloen@users.noreply.github.com>
2023-11-08 16:34:20 +0800

    chore(types): update shims.d.ts

--- src/types/global..d.ts
+++ src/types/global..d.ts
@@ -3,4 +3,5 @@ declare const IS_PROD: boolean
 declare const IS_DEV: boolean
 declare const IS_BUILD: boolean
 
+// use var to declare globalThis / window variable
 declare var __REACT_DEVTOOLS_GLOBAL_HOOK__: boolean
--- src/types/shims.d.ts
+++ src/types/shims.d.ts
@@ -1,3 +1,10 @@
 /**
  * declare file types, *.png *.gif
  */
+
+
+// if vite-plugin-svgr
+// declare module '*.svg?react' {
+//   const ReactComponent: React.FunctionComponent<React.ComponentProps<'svg'> & { title?: string }>
+//   export default ReactComponent
+// }
