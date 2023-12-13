bac42666957daf9d7fc5109656615224778464d9
N <34268416+ziloen@users.noreply.github.com>
2023-12-13 10:55:19 +0800

    chore: update unocss config

--- unocss.config.ts
+++ unocss.config.ts
@@ -1,15 +1,16 @@
 import {
   defineConfig,
-  presetAttributify,
   presetIcons,
   presetUno,
   presetWebFonts,
-  transformerDirectives
-  // transformerVariantGroup,
+  transformerDirectives,
+  transformerVariantGroup,
 } from 'unocss'
 import { presetZiloen } from 'unocss-preset-ziloen'
+import type { Theme } from 'unocss/preset-uno'
 
-export default defineConfig({
+// eslint-disable-next-line @typescript-eslint/no-unnecessary-type-arguments
+export default defineConfig<Theme>({
   shortcuts: [
     ['btn', 'px-4 py-1 rounded inline-block bg-teal-600 text-white cursor-pointer hover:bg-teal-700 disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50'],
     ['icon-btn', 'text-[0.9em] inline-block cursor-pointer select-none opacity-75 transition duration-200 ease-in-out hover:opacity-100 hover:text-teal-600']
@@ -17,7 +18,6 @@ export default defineConfig({
   presets: [
     presetZiloen(),
     presetUno(),
-    // presetAttributify(),
     presetIcons({
       scale: 1.2,
       warn: true
@@ -32,7 +32,7 @@ export default defineConfig({
   ],
   transformers: [
     // support @apply
-    transformerDirectives()
-    // transformerVariantGroup(),
+    transformerDirectives(),
+    transformerVariantGroup(),
   ]
 })
