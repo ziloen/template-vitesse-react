674d98d0dcf683c9f17f03e0fd82c0551f54bba4
N <34268416+ziloen@users.noreply.github.com>
2024-01-09 21:03:17 +0800

    chore(deps): update

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,7 @@
 import { format, react } from "@ziloen/eslint-config"
 
 
-/** @type { import("@ziloen/eslint-config").FlatESLintConfigItem[] } */
+/** @type { import("@ziloen/eslint-config").FlatESLintConfig[] } */
 export default [
   ...react({ tsconfigPath: "./tsconfig.json" }),
   ...format
