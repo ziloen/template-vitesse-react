08f6ba616c142b7825e12be96976afe93e3ef12c
N <34268416+ziloen@users.noreply.github.com>
2024-02-21 16:55:19 +0800

    types: update react useEffect type

--- src/types/modules.d.ts
+++ src/types/modules.d.ts
@@ -10,7 +10,9 @@ declare namespace React {
   interface CSSProperties {
     [CSSCutomProperties: `--${string}`]: string | number | undefined
   }
-  
+
   // allow destructor return value
   function useEffect(effect: () => (void | (() => void)), deps?: DependencyList): void
+  function useInsertionEffect(effect: () => void | (() => void), deps?: DependencyList): void
+  function useLayoutEffect(effect: () => void | (() => void), deps?: DependencyList): void
 }
