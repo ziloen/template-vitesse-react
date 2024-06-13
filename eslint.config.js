1fbe739d6cd91a18eb35733f5d49c164a8de314b
N <34268416+ziloen@users.noreply.github.com>
2024-06-13 18:36:58 +0800

    Update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,9 @@
 import { react } from '@ziloen/eslint-config'
 
 /** @type { import("@ziloen/eslint-config").FlatESLintConfig[] } */
-export default [...react()]
+export default [...react({
+  project: [
+    "./tsconfig.json",
+    "./tsconfig.node.json"
+  ]
+})]
