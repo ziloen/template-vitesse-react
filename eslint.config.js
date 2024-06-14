45ebe7b957dd7978e073210f407606d9b9756aa4
N <34268416+ziloen@users.noreply.github.com>
2024-06-14 13:49:41 +0800

    Format code

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,11 @@
 import { react } from '@ziloen/eslint-config'
 
 /** @type { import("@ziloen/eslint-config").FlatESLintConfig[] } */
-export default [...react({
-  project: [
-    "./tsconfig.json",
-    "./tsconfig.node.json"
-  ]
-})]
+export default [
+  ...react({
+    project: [
+      "./tsconfig.json",
+      "./tsconfig.node.json"
+    ]
+  })
+]
