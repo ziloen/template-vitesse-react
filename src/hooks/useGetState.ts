c607c38b8bc25215ff79a78771b403b28316c4ad
N <34268416+ziloen@users.noreply.github.com>
2025-01-15 17:55:20 +0800

    fix import

--- src/hooks/useGetState.ts
+++ src/hooks/useGetState.ts
@@ -1,3 +1,5 @@
+import { useCallback, useRef, useState } from 'react'
+
 /**
  * @example
  * ```tsx
--- src/hooks/useMagicKeys.ts
+++ src/hooks/useMagicKeys.ts
@@ -1,4 +1,5 @@
 // https://vueuse.org/core/useMagicKeys/
+import { useMemo } from 'react'
 
 export function useMagicKeys() {
   const usedKeys = useMemo(() => new Set<string>(), [])
