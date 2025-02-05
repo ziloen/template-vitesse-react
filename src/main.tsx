32ba84c69f60376ac57019d16a68b835e78edade
N <34268416+ziloen@users.noreply.github.com>
2025-02-06 06:14:37 +0800

    update deps

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,13 @@
 import { createRoot } from 'react-dom/client'
+import { scan } from 'react-scan'
 import App from './App'
 
 import './styles/tailwind.css'
 
 import './styles/main.css'
+
+if (import.meta.env.DEV) {
+  scan({ enabled: true })
+}
 
 createRoot(document.querySelector('#root')!).render(<App />)
