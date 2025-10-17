7065adebbf51a6466a874e69812bc55e794c63d7
N <34268416+ziloen@users.noreply.github.com>
2025-10-17 14:49:41 +0800

    chore: remove tsgo

--- src/types/global..d.ts
+++ src/types/global..d.ts
@@ -1,4 +1,4 @@
-/* eslint-disable no-var */
+/* eslint-disable @typescript-eslint/no-redeclare */
 /**
  * @deprecated
  * use `import.meta.env.PROD` instead
@@ -15,7 +15,49 @@ declare const IS_PROD: boolean
  */
 declare const IS_DEV: boolean
 
+// use const to declare direct global constants
 declare const IS_BUILD: boolean
 
-// use var to declare globalThis / window variable
+// use var to declare direct / globalThis / window variables
 declare var __REACT_DEVTOOLS_GLOBAL_HOOK__: boolean | undefined
+
+interface LanguageDetector {
+  readonly expectedInputLanguages: string[] | null
+
+  readonly inputQuota: number
+
+  destroy(): void
+
+  detect(
+    input: string,
+    options?: { signal?: AbortSignal },
+  ): Promise<
+    {
+      /**
+       * A BCP 47 language tag representing the detected language.
+       */
+      detectedLanguage: string
+      /**
+       * A number between 0 and 1 representing the AI model's confidence that the detected language is correct.
+       */
+      confidence: number
+    }[]
+  >
+}
+
+declare var LanguageDetector: {
+  create(options: {
+    /**
+     * An array of strings specifying the expected languages of the input text, which helps improve the accuracy of the language detection. These should be valid BCP 47 language tags. Defaults to ["en"].
+     */
+    expectedInputLanguages: string[]
+    /**
+     * An AbortSignal object instance, which allows the create() operation to be aborted via the associated AbortController.
+     */
+    signal?: AbortSignal
+  }): Promise<LanguageDetector>
+
+  availability(options: {
+    expectedInputLanguages: string[]
+  }): Promise<'available' | 'downloadable' | 'downloading' | 'unavailable'>
+}
---
+++ src/types/test.ts
@@ -0,0 +1,14 @@
+/* eslint-disable @typescript-eslint/no-unused-expressions */
+__REACT_DEVTOOLS_GLOBAL_HOOK__
+globalThis.__REACT_DEVTOOLS_GLOBAL_HOOK__
+window.__REACT_DEVTOOLS_GLOBAL_HOOK__
+
+IS_BUILD
+// @ts-expect-error IS_BUILD should not be on globalThis
+globalThis.IS_BUILD
+// @ts-expect-error IS_BUILD should not be on window
+window.IS_BUILD
+
+LanguageDetector.create({ expectedInputLanguages: [] })
+globalThis.LanguageDetector
+window.LanguageDetector
